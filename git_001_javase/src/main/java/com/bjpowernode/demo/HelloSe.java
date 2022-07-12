66c1c77ea1384bf7dc27f648d404c4e4e1037f63
zar <zar@163.com>
2022-07-12 09:34:29 +0800

    新程序员增加的内容

--- git_001_javase/src/main/java/com/bjpowernode/demo/HelloSe.java
+++ git_001_javase/src/main/java/com/bjpowernode/demo/HelloSe.java
@@ -4,5 +4,6 @@ public class HelloSe {
     public static void main(String[] args) {
         System.out.println("这是javaSE的项目............");
         System.out.println("我是新增加的内容............");
+        System.out.println("我是新入职的程序员..........");
     }
 }
