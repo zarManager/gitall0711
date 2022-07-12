b7b288e9cd8272389d781a9c8430b487d210e49d
zar <zar@163.com>
2022-07-12 09:08:32 +0800

    修改了HelloSe类中的内容

--- git_001_javase/src/main/java/com/bjpowernode/demo/HelloSe.java
+++ git_001_javase/src/main/java/com/bjpowernode/demo/HelloSe.java
@@ -3,5 +3,6 @@ package com.bjpowernode.demo;
 public class HelloSe {
     public static void main(String[] args) {
         System.out.println("这是javaSE的项目............");
+        System.out.println("我是新增加的内容............");
     }
 }
